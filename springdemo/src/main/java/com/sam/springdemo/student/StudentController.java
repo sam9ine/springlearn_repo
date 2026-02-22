cb93ee4ad391be3992cb13b9421823c1b4305424
sameer <sameerr1105@gmail.com>
2026-02-22 17:04:47 +0530

    added controller

---
+++ springdemo/src/main/java/com/sam/springdemo/student/StudentController.java
@@ -0,0 +1,22 @@
+package com.sam.springdemo.student;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<String> findAllStudents(){
+        return List.of(
+                "hey",
+                "this is",
+                "my first method",
+                "in spring"
+        );
+
+    }
+}
